9cd924f47fc21a648d471257635247a7f62bc9eb
Yuerino <50553280+Yuerino@users.noreply.github.com>
2022-05-11 02:29:54 +0200

    feat: initialise new player struct and update var name accordingly

--- srcs/input_handler/init.c
+++ srcs/input_handler/init.c
@@ -6,7 +6,7 @@
 /*   By: cthien-h <cthien-h@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 22:22:27 by cthien-h          #+#    #+#             */
-/*   Updated: 2022/05/10 17:06:46 by cthien-h         ###   ########.fr       */
+/*   Updated: 2022/05/10 23:00:07 by cthien-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,19 +30,38 @@ static void	init_mlx(t_cub3d *data)
  */
 static void	init_map(t_map *map)
 {
-	map->south.img = NULL;
-	map->south.img = NULL;
-	map->west.img = NULL;
-	map->east.img = NULL;
-	map->map = NULL;
+	map->north.img_ptr = NULL;
+	map->south.img_ptr = NULL;
+	map->west.img_ptr = NULL;
+	map->east.img_ptr = NULL;
+	map->floor_color[0] = -1;
+	map->ceiling_color[0] = -1;
+	map->data = NULL;
 	map->height = -1;
 }
 
+/**
+ * @brief Initialise player struct
+ */
+static void	init_player(t_player *player)
+{
+	player->x = -1;
+	player->y = 1;
+	player->dir_x = 0.0f;
+	player->dir_y = 0.0f;
+}
+
+/**
+ * @brief Initialise cub3d main struct and its elements
+ * and read map data from file *
+ * @param filename Path to map file
+ */
 void	init_cub3d(t_cub3d *data, char *filename)
 {
 	init_mlx(data);
 	init_map(&data->map);
-	read_map(data, filename);
+	init_player(&data->player);
+	read_file_data(data, filename);
 }
 
 /**
@@ -52,19 +71,18 @@ void	init_cub3d(t_cub3d *data, char *filename)
  */
 void	free_data(t_cub3d *data)
 {
-	if (data->map.north.img)
-		mlx_destroy_image(data->mlx, data->map.north.img);
-	if (data->map.south.img)
-		mlx_destroy_image(data->mlx, data->map.south.img);
-	if (data->map.west.img)
-		mlx_destroy_image(data->mlx, data->map.west.img);
-	if (data->map.east.img)
-		mlx_destroy_image(data->mlx, data->map.east.img);
-	while (data->map.map && data->map.height-- >= 0)
-		free(data->map.map[data->map.height]);
-	if (data->map.map)
-		free(data->map.map);
+	if (data->map.north.img_ptr)
+		mlx_destroy_image(data->mlx, data->map.north.img_ptr);
+	if (data->map.south.img_ptr)
+		mlx_destroy_image(data->mlx, data->map.south.img_ptr);
+	if (data->map.west.img_ptr)
+		mlx_destroy_image(data->mlx, data->map.west.img_ptr);
+	if (data->map.east.img_ptr)
+		mlx_destroy_image(data->mlx, data->map.east.img_ptr);
+	if (data->map.data)
+		free_carray(data->map.data);
 	if (data->win)
 		mlx_destroy_window(data->mlx, data->win);
 	mlx_destroy_display(data->mlx);
+	free(data->mlx);
 }
