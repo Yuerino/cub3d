1c02df9951a72b20450f8e541668462d41da105f
Yuerino <50553280+Yuerino@users.noreply.github.com>
2022-05-11 02:24:26 +0200

    feat: update and add more error helper utils

--- utils/error.c
+++ utils/error.c
@@ -6,7 +6,7 @@
 /*   By: cthien-h <cthien-h@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 18:55:02 by cthien-h          #+#    #+#             */
-/*   Updated: 2022/05/10 18:54:47 by cthien-h         ###   ########.fr       */
+/*   Updated: 2022/05/10 21:14:37 by cthien-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,37 @@
 
 /**
  * @brief Print the error message to standard error
+ * and (optional) free cub3d data
  * @param err Error message
- * @return 1 for error
+ * @param data (Optional) Cub3d's data to be freed
+ * @return 0 for error
  */
-int	print_error(char *err)
+int	print_error(char *err, t_cub3d *data)
 {
 	ft_putendl_fd(err, STDERR_FILENO);
-	return (1);
+	if (data)
+		free_data(data);
+	return (0);
 }
 
 /**
  * @brief Print the errno message with (optional) error message
+ * and (optional) free cub3d data
  * @param err (Optional) Error message
- * @return 1 for error
+ * @param data (Optional) Cub3d's data to be freed
+ * @return 0 for error
  */
-int	print_perror(char *err)
+int	print_perror(char *err, t_cub3d *data)
 {
 	perror(err);
-	return (1);
+	if (data)
+		free_data(data);
+	return (0);
 }
 
 /**
- * @brief Print the error message and (optional) free data then exit error
+ * @brief Print the error message and (optional)
+ * free cub3d data then exit error
  * @param err Error message
  * @param data (Optional) Cub3d's data to be freed
  */
@@ -49,7 +58,7 @@ void	exit_error(char *err, t_cub3d *data)
 
 /**
  * @brief Print errno message with (optional) error message and (optional)
- * free data then exit error
+ * free cub3d data then exit error
  * @param err (Optional) Error message
  * @param data (Optional) Cub3d's data to be freed
  */
