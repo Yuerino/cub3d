9d46745f3909dc2f79a8f2ee70426bb003a2c663
Yuerino <50553280+Yuerino@users.noreply.github.com>
2022-05-11 02:27:00 +0200

    feat(dev): X button to free data and gracefully exit

--- cub3d.c
+++ cub3d.c
@@ -6,11 +6,22 @@
 /*   By: cthien-h <cthien-h@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 18:50:13 by cthien-h          #+#    #+#             */
-/*   Updated: 2022/05/10 00:19:34 by cthien-h         ###   ########.fr       */
+/*   Updated: 2022/05/10 23:40:57 by cthien-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+/**
+ * @brief Free data and exit program
+ * @note For development usage, delete or move it later
+ */
+static int	close_game(t_cub3d *data)
+{
+	free_data(data);
+	exit(EXIT_SUCCESS);
+	return (0);
+}
 
 int	main(int argc, char **argv)
 {
@@ -19,7 +30,7 @@ int	main(int argc, char **argv)
 	if (argc != 2)
 		exit_error("Usage: ./cub3d [map file]", NULL);
 	init_cub3d(&data, argv[1]);
+	mlx_hook(data.win, 17, 0, close_game, &data);
 	mlx_loop(data.mlx);
-	free_data(&data);
 	return (0);
 }
