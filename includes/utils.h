39c8024196c1d384b26dc1ef8ac1e63231ed1a5a
Yuerino <50553280+Yuerino@users.noreply.github.com>
2022-05-11 02:30:41 +0200

    feat: update utils header with new utils

--- includes/utils.h
+++ includes/utils.h
@@ -6,27 +6,29 @@
 /*   By: cthien-h <cthien-h@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/10 00:11:37 by cthien-h          #+#    #+#             */
-/*   Updated: 2022/05/10 18:55:12 by cthien-h         ###   ########.fr       */
+/*   Updated: 2022/05/10 23:08:19 by cthien-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef UTILS_H
 # define UTILS_H
 
-#include "libft.h"
+# include "libft.h"
 
 typedef struct s_cub3d	t_cub3d;
 
 void	*image_dup(void *mlx, void *img);
 int		is_color_valid(int *color);
-int		is_ext_valid(char *filename);
+int		is_ext_valid(char *filename, char *ext);
+int		is_nbr(char *s);
 
-int		print_error(char *err);
-int		print_perror(char *err);
+int		print_error(char *err, t_cub3d *data);
+int		print_perror(char *err, t_cub3d *data);
 void	exit_error(char *err, t_cub3d *data);
 void	exit_perror(char *err, t_cub3d *data);
 
 void	free_carray(char **array);
 int		carray_len(char **array);
+char	**carray_expand(char **array);
 
-# endif
+#endif
